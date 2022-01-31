247249f1d0a133a68473a1ef954773ec8fb21957
Barış Elvanoğlu <bariselvn@hotmail.com>
2022-01-31 05:33:28 +0300

    adding smart contract

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,9 +1,12 @@
 
+import React, { useContext } from "react";
 import { AiFillAlipayCircle } from "react-icons/ai";
 import { SiEthereum } from "react-icons/si";
 import { BsInfoCircle } from "react-icons/bs";
 
+import { TransactionContext } from "../context/TransactionContext";
 import { Loader } from './';
+import { useState } from "react";
 
 const commonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white"
 
@@ -20,14 +23,17 @@ const Input = ({ placeholder, name, type, value, handleChange }) => (
 );
 
 const Welcome = () => {
-    const connectWallet = () => {
+    const { connectWallet, currentAccount, formData, handleChange, sendTransaction } = useContext(TransactionContext);
 
+    const handleSubmit = (e) => {
+        const { addressTo, amount, keyword, message } = formData;
 
+        e.preventDefault();
+
+        if (!addressTo || !amount || !keyword || !message) return;
+
+        sendTransaction();
     }
-    const handleSubmit = () => {
-
-    }
-
 
     return (
         <div className="flex w-full justify-center items-center">
@@ -39,13 +45,16 @@ const Welcome = () => {
                     <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
                         Explore the crypto world. Buy and sell crpytocurrencies easily on Krypto.
                     </p>
-                    <button
-                        type="button"
-                        onClick={connectWallet}
-                        className=" flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"
-                    >
-                        <p className="text-white tex-base font-semibold">Connect Wallet</p>
-                    </button>
+                    {!currentAccount && (
+                        <button
+                            type="button"
+                            onClick={connectWallet}
+                            className=" flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"
+                        >
+                            <p className="text-white tex-base font-semibold">Connect Wallet</p>
+                        </button>
+                    )}
+
 
                     <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
                         <div className={`rounded-tl-2xl ${commonStyles}`}>
@@ -84,10 +93,10 @@ const Welcome = () => {
                         </div>
                     </div>
                     <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
-                        <Input placeholder="Address To" name="addressTo" type="text" handleChange={() => {}} />
-                        <Input placeholder="Amount (ETH)" name="amount" type="number" handleChange={() => {}} />
-                        <Input placeholder="Keyword (Gif)" name="keyword" type="text" handleChange={() => {}} />
-                        <Input placeholder="Enter Message" name="message" type="text" handleChange={() => {}} />
+                        <Input placeholder="Address To" name="addressTo" type="text" handleChange={handleChange} />
+                        <Input placeholder="Amount (ETH)" name="amount" type="number" handleChange={handleChange} />
+                        <Input placeholder="Keyword (Gif)" name="keyword" type="text" handleChange={handleChange} />
+                        <Input placeholder="Enter Message" name="message" type="text" handleChange={handleChange} />
 
                         <div className="h-[1px] w-full bg-gray-400 my-2" />
 
---
+++ client/src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from './Transactions.json'
+
+export const contractABI = abi.abi ;
+export const contractAddress = '0x18368b9645cF9E24649B9a70fF8C2E2D69e08924';
